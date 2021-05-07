e0001bd98cd3e05e82b747b408bb0d5b993dab54
Sebastian Pizarro <jsebastianpizarro@gmail.com>
2021-05-07 05:45:30 -0300

    Se agrego la funcionalidad Cargar Personas y un menú

--- src/com/paradigmas/unidad1/practico1/Main.java
+++ src/com/paradigmas/unidad1/practico1/Main.java
@@ -17,15 +17,63 @@ public class Main {
 //        System.out.println("Input user:"+ inputValue);
 
         // Declarar una Variable
-        Persona persona1;
-        Persona persona2;
+        int cantidadPersonas;
+        String auxNombre;
+        String auxApellido;
+        int opcion=0;
+        
+        
 
         //Instanciar
-        persona1 = new Persona();
-        persona2 = new Persona("Juan", "Perez");
+       // persona1 = new Persona();
+       // persona2 = new Persona("Juan", "Perez");
+        Scanner sc = new Scanner(System.in);
+        Scanner scLetras = new Scanner(System.in);
+        
+        
 
-        System.out.println(persona2);
+        
+        System.out.print("Cantidad de personas que desea cargar: ");
+        cantidadPersonas = sc.nextInt();
+        Persona[] personas = new Persona[cantidadPersonas];
+        
+        
+        for(int i=0;i<cantidadPersonas;i++){
+            System.out.println("\n Cargar persona " +(i+1));
+            System.out.print("Nombre : ");
+            auxNombre = scLetras.nextLine();
+            System.out.print("Apellido : ");
+            auxApellido = scLetras.nextLine();
+            personas[i] = new Persona(auxNombre,auxApellido);
+        }
+        
+        
+         //MENU
+        System.out.println(" ");
+        System.out.println(" ");
+        System.out.println("ELIJA LA OPERACION DESEADA :");
+        System.out.println(" ");
+        System.out.println("1.Cargar Personas");
+        System.out.println("2.Cargar Equipos");
+        System.out.println("3.Cargar Canchas");
+        System.out.println("4.Mostrar datos cargados");
+        System.out.println("5.Salir");
+        System.out.println(" ");
+        opcion = sc.nextInt();
+        System.out.println(" ");
+        System.out.println(" ");
+        System.out.println(" ");
+        System.out.println(" ");
+        System.out.println(" ");
+        
 
 
+    
+    
+    
+    
+    
+    
+    
     }
 }
