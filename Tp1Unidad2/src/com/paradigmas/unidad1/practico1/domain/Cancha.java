acb6e959db04531f8026c5079b5ee0da0a9706c7
darkeruz <darkeruz@gmail.com>
2021-05-07 03:22:49 -0300

    agregado el constructor de la clase Cancha y sus respectivos Getters and setters.

--- Tp1Unidad2/src/com/paradigmas/unidad1/practico1/domain/Cancha.java
+++ Tp1Unidad2/src/com/paradigmas/unidad1/practico1/domain/Cancha.java
@@ -4,7 +4,32 @@ public class Cancha {
 
     private String nombre;
 
-    private Integer capacidadEspectores;
+    private Integer capacidadEspectadores;
+    
+    //Constructor
+    
+    public Cancha(String nombre, Integer capacidadEspectadores){
+        this.nombre = nombre;
+        this.capacidadEspectadores = capacidadEspectadores;
+    }
+    
+    //Getters and Setters
+    
+    public String getNombre(){
+        return nombre;
+    }
+    
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    
+    public Integer getCapacidadEspectadores(){
+        return capacidadEspectadores;
+    }
+    
+    public void setCapacidadEspectadores(Integer capacidadEspectadores){
+        this.capacidadEspectadores = capacidadEspectadores;
+    }
 
 
 }
